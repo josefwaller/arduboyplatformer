cd4f84dc04528db06f72ecff06da0f735e48f569
Josef Waller <josef@siriusapplications.com>
2019-09-22 22:48:26 -0700

    Draw test images

--- ArduboyPlatformer.ino
+++ ArduboyPlatformer.ino
@@ -1,6 +1,14 @@
+#include <Arduboy2.h>
+#include "test.h"
+
+Arduboy2 a;
 
 void setup() {
+	a.begin();
 }
 
 void loop() {
+	a.clear();
+	a.drawBitmap(0, 0, test_bin, 16, 48, WHITE);
+	a.display();
 }
