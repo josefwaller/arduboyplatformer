b38bdc01740be95188b275a5dfac4bc4a72f9e44
Josef Waller <josef@siriusapplications.com>
2019-09-23 13:48:19 -0700

    Use external masks

--- ArduboyPlatformer.ino
+++ ArduboyPlatformer.ino
@@ -1,14 +1,27 @@
 #include <Arduboy2.h>
 #include "test.h"
+#include "test_mask.h"
 
 Arduboy2 a;
+Sprites s;
+long lastMillis;
+int counter;
 
 void setup() {
 	a.begin();
+  lastMillis = millis();
 }
+
 
 void loop() {
 	a.clear();
-	a.drawBitmap(0, 0, test_bin, 16, 48, WHITE);
+  for (size_t x = 0; x < 10; x++) {
+    a.drawFastHLine(0, 10 * x, 200);
+  }
+  for (size_t y = 0; y < 16; y++) {
+    a.drawFastVLine(16 * y, 0, 100);
+  }
+  s.drawExternalMask(0, 16, test_bin, test_mbin, 0, 0);
+  a.print(counter);
 	a.display();
 }
