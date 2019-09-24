47842929adf38188944c449726aa216445dc017b
Josef Waller <josef@siriusapplications.com>
2019-09-24 11:38:51 -0700

    Move cpp files to top level for compilation

--- upload.sh
+++ upload.sh
@@ -1,2 +1,4 @@
+find -name "*.cpp" -exec cp {} . \;
 arduino-cli compile --fqbn arduino:avr:leonardo --build-cache-path ./cache/
+rm *.cpp
 cmd.exe /C start ArduboyPlatformer.arduino.avr.leonardo.hex
