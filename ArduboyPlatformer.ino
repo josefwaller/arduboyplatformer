72a4d245583a35675566e8d1200ffc920e9c4d35
Josef Waller <josef@siriusapplications.com>
2019-09-24 10:22:29 -0700

    Draw tilemap

--- ArduboyPlatformer.ino
+++ ArduboyPlatformer.ino
@@ -1,6 +1,7 @@
 #include <Arduboy2.h>
 #include "player.h"
 #include "tileset.h"
+#include "test_map.h"
 
 Arduboy2 a;
 Sprites s;
@@ -20,6 +21,17 @@ void loop() {
   }
   for (size_t y = 0; y < 16; y++) {
     a.drawFastVLine(16 * y, 0, 100);
+  }
+  // Draw the tile map slowly
+  for (size_t y = 0; y < 5; y++) {
+    for (size_t x = 0; x < 10; x++) {
+      uint8_t tile_index = test_map[y * 16 + x];
+      // Check that the tile is not -1 (i.e. no tile)
+      if (tile_index != 0xFF) {
+        s.drawOverwrite(x * 16, y * 16, tileset, tile_index);
+        a.setCursor(x * 16, y * 16);
+      }
+    }
   }
   s.drawExternalMask(0, 16, player, player_mask, counter, counter);
   if (millis() - lastMillis >= 250) {
