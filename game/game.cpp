785a3d13ba062ceda6cb05e4b3460e9daa93fa58
Josef Waller <josef@siriusapplications.com>
2019-10-16 09:49:40 -0700

    Tidy up game.cpp code a bit

--- game/game.cpp
+++ game/game.cpp
@@ -14,14 +14,19 @@ void beginGame(Game* g, Arduboy2* a) {
   initLevel(g);
 }
 void updateGame(Game* g, Sprites* s) {
+  // Get some values to avoid having g-> everywhere
+  Player* p = &g->p;
+  Info* info = &g->info;
+  WalkingEnemy* we = &g->we;
   Arduboy2* a = g->info.arduboy;
+  v2 camera = g->info.camera;
   float delta = (millis() - g->lastMillis) / 1000.0f;
   g->lastMillis = millis();
   for (size_t x = 0; x < 10; x++) {
-    a->drawFastHLine(-g->info.camera.x + 0, -g->info.camera.y + 16 * x, 200);
+    a->drawFastHLine(-camera.x + 0, -camera.y + 16 * x, 200);
   }
   for (size_t y = 0; y < 16; y++) {
-    a->drawFastVLine(-g->info.camera.x + 16 * y, -g->info.camera.y + 0, 100);
+    a->drawFastVLine(-camera.x + 16 * y, -camera.y + 0, 100);
   }
   // Draw the tile map slowly
   for (size_t y = 0; y < 5; y++) {
@@ -29,55 +34,55 @@ void updateGame(Game* g, Sprites* s) {
       uint8_t tile_index = g->info.map[y * 16 + x];
       // Check that the tile is not -1 (i.e. no tile)
       if (tile_index != 0xFF) {
-        s->drawOverwrite(x * 16 - g->info.camera.x, y * 16 - g->info.camera.y, tileset, tile_index);
+        s->drawOverwrite(x * 16 - camera.x, y * 16 - camera.y, tileset, tile_index);
       }
     }
   }
-  if (g->p.bb.pos.x > 30) {
+  if (p->bb.pos.x > 30) {
     g->info.camera.x = g->p.bb.pos.x - 30;
   } else {
     g->info.camera.x = 0;
   }
-  if (g->p.isDead) {
+  if (p->isDead) {
     a->setCursor((WIDTH - (9 * 6)) / 2, HEIGHT / 2 - 7);
     a->print("You died.");
     a->setCursor((WIDTH - (11 * 6)) / 2, HEIGHT / 2);
     a->print("Start Over?");
     if (a->pressed(A_BUTTON)) {
       initLevel(g);
-      g->p.isDead = false;
     }
   }
-  updatePlayer(&g->p, &g->info, delta);
-  updateWalkingEnemy(&g->we, &g->info, delta);
+  updatePlayer(p, info, delta);
+  updateWalkingEnemy(we, info, delta);
   for (uint8_t i = 0; i < POWERUP_ARRAY_SIZE; i++) {
     if (g->powerUpExists[i]) {
-      updatePowerUp(&g->powerUps[i], &g->info, delta);
+      updatePowerUp(&g->powerUps[i], info, delta);
     }
   }
   // Check for collisions
-  if (collides(&g->p.bb, &g->we.bb) && !g->we.isDead && !g->p.isDead) {
-    if (g->p.bb.pos.y - g->we.bb.pos.y < -5) {
-      g->we.isDead = true;
-      g->p.vel.y = -40;
+  // This should really be in a function eventually
+  if (collides(&p->bb, &we->bb) && !we->isDead && !p->isDead) {
+    if (p->bb.pos.y - we->bb.pos.y < -5) {
+      we->isDead = true;
+      p->vel.y = -40;
       if (a->pressed(A_BUTTON)) {
-        g->p.vel.y = -100;
+        p->vel.y = -100;
       }
     } else {
-      g->p.isDead = true;
+      p->isDead = true;
     }
   }
   a->setCursor(0, 0);
   a->print(delta * 1000.0f);
-  drawPlayer(&g->p, &g->info);
-  drawWalkingEnemy(&g->we, &g->info);
+  drawPlayer(p, info);
+  drawWalkingEnemy(we, info);
   for (uint8_t i = 0; i < POWERUP_ARRAY_SIZE; i++) {
     if (g->powerUpExists[i]) {
-      drawPowerUp(&g->powerUps[i], &g->info);
+      drawPowerUp(&g->powerUps[i], info);
     }
   }
-
 }
+
 void initLevel(Game* g) {
   v2 playerPos = { 33, 16 };
   g->p = createPlayer(playerPos);
@@ -93,3 +98,4 @@ void initLevel(Game* g) {
   g->info.map = g->level;
   g->info.player = &g->p;
 }
+
